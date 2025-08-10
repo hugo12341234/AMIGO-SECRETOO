7f91e7321c6f7f9ea167a66577264daf6db66eb6
Hugo Chacon <hugochaconjaral@gmail.com>
2025-08-09 23:40:54 -0600

    actualiza la app

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+function agregarAmigo (){
+    nombre = (document.getElementById('nombreAmigo').value);
+
+    if (nombre){                                //in JS an empty string means false, this is why this works
+        listaDeAmigos.push(nombre);
+        limpiarCaja();
+        //console.log(listaDeAmigos);
+    } else {
+        alert ('Da un nombre valido');
+    }
+
+    return;
+}
+
+function limpiarCaja (){
+    document.getElementById('nombreAmigo').value = ""
+}
