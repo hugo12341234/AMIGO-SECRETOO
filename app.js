896bcd07d0f61123da08a001433deeb26dd598aa
Hugo Chacon <hugochaconjaral@gmail.com>
2025-08-19 21:31:52 -0600

    actualiza el app.js

--- app.js
+++ app.js
@@ -1,19 +1,51 @@
-let listaDeAmigos = [];
 
-function agregarAmigo (){
-    nombre = (document.getElementById('nombreAmigo').value);
+let amigos = [];
 
-    if (nombre){                                //in JS an empty string means false, this is why this works
-        listaDeAmigos.push(nombre);
-        limpiarCaja();
-        //console.log(listaDeAmigos);
-    } else {
-        alert ('Da un nombre valido');
-    }
-
-    return;
+function limpiarInput(elemento) {
+   return document.querySelector(elemento).value = '';
 }
 
-function limpiarCaja (){
-    document.getElementById('nombreAmigo').value = ""
+function actualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function agregarAmigo() {
+    let input = document.getElementById('amigo');
+    let nombre = input.value.trim();
+
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+
+    amigos.push(nombre);
+    actualizarLista();
+    limpiarInput('#amigo');
+}
+
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('No hay amigos para sortear');
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    let amigoSorteado = amigos[indiceAleatorio];
+    amigos.splice(indiceAleatorio, 1);
+
+    let listaResultado = document.getElementById('resultado');
+    listaResultado.innerHTML = ""; 
+    let li = document.createElement('li');
+    li.textContent = amigoSorteado;
+    listaResultado.appendChild(li);
 }
